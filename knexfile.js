b75d44866f70b3674708383640b4e1cb07798e76
Nate Boyette <nateboyette@icloud.com>
2019-03-07 10:57:22 -0800

    heroku update

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,14 @@
 // Update with your config settings.
 
+const localPGConnection = {
+  host: 'localhost',
+  database: 'lambda',
+  user: 'student',
+  'password': 'hired'
+}
+
+const prodDbConnection = process.env.DATABASE_URL || localPGConnection;
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -27,4 +36,14 @@ module.exports = {
       directory: './data/seeds',
     },
   },
+  production: {
+    client: 'pg',
+    connection: prodDbConnection, // an object or a string
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  }
 };
