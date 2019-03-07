9d41af408098f90739c7ad1dceab16292e2d7e3f
Nate Boyette <nateboyette@icloud.com>
2019-03-07 11:05:32 -0800

    update

--- knexfile.js
+++ knexfile.js
@@ -1,49 +1,51 @@
 // Update with your config settings.
 
+
+
 const localPGConnection = {
-  host: 'localhost',
-  database: 'lambda',
-  user: 'student',
-  'password': 'hired'
-}
+  host: "localhost",
+  database: "lambda",
+  user: "student",
+  password: "hired"
+};
 
 const prodDbConnection = process.env.DATABASE_URL || localPGConnection;
 
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/hobbits.db3',
+      filename: "./data/hobbits.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
-    },
+      directory: "./data/seeds"
+    }
   },
   testing: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/test.db3',
+      filename: "./data/test.db3"
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
-    },
+      directory: "./data/seeds"
+    }
   },
   production: {
-    client: 'pg',
+    client: "pg",
     connection: prodDbConnection, // an object or a string
     migrations: {
-      directory: './data/migrations',
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds',
-    },
+      directory: "./data/seeds"
+    }
   }
 };
